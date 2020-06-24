42f035cee43b7e0bf03f6ddc35a7a725b133b3e5
itls <1144621678@qq.com>
2020-06-24 10:55:47 +0800

    完成了登录和注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,6 +13,7 @@ $(function () {
 
     // 从layui中获取form对象
     var form = layui.form;
+    var layer = layui.layer;
     // 通过form.verify() 函数自定义校验规则
     form.verify({
         // 自定义了一个叫做 pwd 校验规则
@@ -28,5 +29,41 @@ $(function () {
                 return '两次密码不一致！'
             }
         }
+    })
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault()
+        var data = {
+            username: $('#form_reg [name = username]').val(),
+            password: $('#form_reg [name = password]').val()
+        }
+        $.post('/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录！')
+            // 模拟人的点击行为
+            $('#link_login').click()
+        })
+    })
+    // 监听登录表单的提交事件
+    // 监听注册表单的提交事件
+    $('#form_login').submit(function (e) {
+        e.preventDefault()
+        $.ajax({
+            type: 'POST',
+            url: '/api/login',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = '../../index.html'
+            }
+        })
     })
 })
